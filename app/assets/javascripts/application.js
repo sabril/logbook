5010edfdb507febacbdae9f1ff45dcce0a3f9a05
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-04-17 13:18:39 +0700

    remove js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-scrollspy
-//= require bootstrap-modal
-//= require bootstrap-dropdown
 //= require_tree .
-
-$('.dropdown-toggle').dropdown();
