f8b591d3d5c54319179ae59ffa38e5aea771f02c
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-04-17 18:08:57 +0700

    fix

--- app/models/asset_version.rb
+++ app/models/asset_version.rb
@@ -1,7 +1,7 @@
 class AssetVersion < ActiveRecord::Base
   attr_accessible :asset_id, :description, :name, :owner_id, :version_number, :asset_version_file
   belongs_to :asset
-  belongs_to :owner, :class => "AdminUser"
+  #belongs_to :owner, :class => "AdminUser"
   # 
   # mount_uploader :asset_version_file, AssetVersionUploader
   
